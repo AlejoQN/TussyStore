ad14a0dacf3b91fbbf0af552a036ae68f4a947dc
Alejandro <duvanalejandroc808@gmail.com>
2025-07-01 20:52:50 -0500

    Soluciones y modificaciones para el despliegue del proyecto

--- backend/server.js
+++ backend/server.js
@@ -35,7 +35,7 @@ const dbConfig = {
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306,
+  port: process.env.DB_PORT || 48457,
 };
 const pool = mysql.createPool(dbConfig);
 app.locals.pool = pool;
